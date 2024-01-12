5776ce8bd4e82ad2ecc5ebb8485157ca6bc0e808
Cody <zimquo@gmail.com>
2024-01-12 13:53:16 -0700

    blocks, raw, ore and tool specifications

---
+++ src/main/java/net/cody/cccmod/item/ModCreativeModTabs.java
@@ -0,0 +1,39 @@
+package net.cody.cccmod.item;
+
+import net.cody.cccmod.CCCMod;
+import net.cody.cccmod.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MOD_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CCCMod.MODID);
+
+    public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MOD_TABS.register("tutorial_tab", () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
+            .title(Component.translatable("creativetab.tutorial_tab")).displayItems((pParameters, pOutput) -> {
+                // Modded Item syntax
+                pOutput.accept(ModItems.SAPPHIRE.get());
+                pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+                // Modded Blocks syntax
+                pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+                pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                pOutput.accept(ModBlocks.ENDSTONE_SAPPHIRE_ORE.get());
+
+                // Default Item syntax
+                pOutput.accept(Items.DIAMOND);
+
+            }).build());
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MOD_TABS.register(eventBus);
+    }
+}
